514aacae46085cc80877da1aff7a6dc9d04a7354
“[Krzysztof <[k.chylinski82@gmail.com]>
2022-08-17 12:22:04 +0100

    following advice, added code in SoundScene to unlock audio on android and iphone.

--- SoundScene.js
+++ SoundScene.js
@@ -4,14 +4,24 @@ class SoundScene extends Phaser.Scene {
     }
 
     preload(){
-        this.load.audio('theme2', './audio/theme2.ogg');
+        this.load.audio('theme', './audio/theme.mp3');
     }
 
     create() {
         this.input.once('pointerdown', _ => {
-            gameState.music = this.sound.add('theme2');
-            gameState.music.play();
-            gameState.music.loop = true; 
+            // UNLOCK audio, doesn't work immediately on iphone
+            this.sound.unlock();
+            gameState.music = this.sound.add('theme', {loop: true});
+            
+            // if unlock worked, just play the sound
+            if(!this.sound.locked){
+                gameState.music.play();
+            } 
+            else {  // IF Not wait on unlock event 
+                this.sound.once(Phaser.Sound.Events.UNLOCKED, () => {
+                    gameState.music.play();
+                })
+            }
         });
     }
 
